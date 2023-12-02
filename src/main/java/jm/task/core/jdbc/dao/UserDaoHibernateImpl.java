c9f67912357f72387b30ac96a8c66874d04ba073
Alexandr A <alexandr179k@gmail.com>
2023-12-02 22:00:28 +0300

    Review: bugs

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -3,19 +3,28 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-    Session session;
+                                //todo: codeStyle - сливается код..
+    //Session session;//todo: благодаря SessionFactory - Session для нас уже не является дорогой операцией и может(и должна) использоваться в каждом методе
+
+    private SessionFactory sessionFactory;
 
     public UserDaoHibernateImpl() {
-        this.session = new Util().getSessionFactory().openSession();
+        sessionFactory = new Util().getSessionFactory();
     }
 
-    String createUsersQuery = "CREATE TABLE IF NOT EXISTS Users " + "(id INTEGER AUTO_INCREMENT, " + " name VARCHAR(255), " + " lastName VARCHAR(255), " + " age TINYINT, " + " PRIMARY KEY ( id ))";
+    private String createUsersQuery = "CREATE TABLE IF NOT EXISTS Users "
+            + "(id INTEGER AUTO_INCREMENT, "
+            + " name VARCHAR(255), "
+            + " lastName VARCHAR(255), "
+            + " age TINYINT, "
+            + " PRIMARY KEY ( id ))";//todo потеряли модификатор
 
     String dropUsersQuery = "DROP TABLE IF EXISTS Users";
 
@@ -25,16 +34,19 @@ public class UserDaoHibernateImpl implements UserDao {
 
 
     @Override
-    public void createUsersTable() {
-        Transaction transaction = session.beginTransaction();
+    public void createUsersTable() {//todo: в кажлом методе используем try_with_resources  - для .openSession();
+//        Transaction transaction = session.beginTransaction();//todo: используем аннотацию Transaction (над класом), параметризированную READONLY - над нужными методами
         Query<?> query = session.createSQLQuery(createUsersQuery);
+        //todo: правильно подавать запрос при работе с Session
+        sessionFactory.getCurrentSession().createQuery(...)
+
         query.executeUpdate();
-        transaction.commit();
+//        transaction.commit();
     }
 
     @Override
     public void dropUsersTable() {
-        Transaction transaction = session.beginTransaction();
+//        Transaction transaction = session.beginTransaction();
         Query<?> query = session.createSQLQuery(dropUsersQuery);
         query.executeUpdate();
         transaction.commit();
@@ -42,6 +54,8 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        //todo:   в продолжении мысли -> например:    sessionFactory.getCurrentSession().save(..);  работаем так, то есть НЕ ЧЕРЕЗ SQL
+
         User user = new User(name, lastName, age);
         Transaction transaction = session.beginTransaction();
         session.save(user);
